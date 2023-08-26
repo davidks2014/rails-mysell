9ecfd11fe1759f11a6604c6f7a5464e4d25c519a
leekoksang <davidks2014@gmail.com>
2023-08-26 16:42:29 +0800

    seed file added

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  CATEGORIES = ["Electronics", "Clothing", "Books", "Home Decor", "Toys"]
+  STATUS = ["Sold", "Available", "Reserved"]
+  belongs_to :user
+  validates :price, presence: true
+  validates :category, presence: true
+  validates :name, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating Users..."
+david = {name: "David", email: "david@gmail.com", password: "123456"}
+teckguan = {name: "Teckguan", email: "teckguan@gmail.com", password: "123456"}
+pingyu = {name: "Pingyu", email: "pingyu@gmail.com", password: "123456"}
+yaya = {name: "Yaya", email: "yaya@gmail.com", password: "123456"}
+
+
+users = [david, teckguan, pingyu, yaya]
+
+users.each do |attributes|
+  user = User.create!(attributes)
+  puts "Created #{user.name}"
+
+  puts " #{user.name} Creating items..."
+  phone = Item.create({name: "phone", price: 5.50, category: "Electronics", user: user })
+  teddybear = Item.create({name: "teddybear", price: 17.50, category: "Toys", user: user })
+  user.items.each do |item|
+    puts "#{item.name}"
+  end
+end
+
+# CATEGORIES = ["Electronics", "Clothing", "Books", "Home Decor", "Toys"]
+puts "Finished!"
