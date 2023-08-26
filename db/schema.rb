33f139ea4aba68b8bb2c2722bf9e00940261af7d
Ping Yu Choi <choipy7@gmail.com>
2023-08-27 06:20:21 +0800

    created 'offer' feature branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_125559) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_071448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "category"
+    t.text "description"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.float "price", null: false
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_offers_on_item_id"
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +45,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_125559) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
+  add_foreign_key "offers", "items"
+  add_foreign_key "offers", "users"
 end
