73f06f67f018b3fd8918764057ff7926cac7f4d2
Zunnur Hidayah Zahri <zhidayah@hotmail.com>
2023-08-27 18:46:47 +0800

    created homepage.css under app/assets/stylesheets, changed colour of the background of the homepage, created an images folder under apps/assets to store images, changed font to montesserat and changed font colours to white

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @featured_products = Product.where(featured: true).limit(4)
   end
 end
