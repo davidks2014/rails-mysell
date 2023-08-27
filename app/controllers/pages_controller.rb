1b3906f963a0abe277657d7e0af26bdb7cc6659a
Zunnur Hidayah Zahri <zhidayah@hotmail.com>
2023-08-27 16:47:59 +0800

    added featured prodcuts code in pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @featured_products = Product.where(featured: true).limit(4)
   end
 end
