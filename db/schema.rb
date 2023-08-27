a11b367436f478c9272cc3e2214f73ccfe5811c5
leekoksang <davidks2014@gmail.com>
2023-08-27 17:05:10 +0800

    set default value to status in item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_174430) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_090325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_174430) do
     t.float "price"
     t.string "category"
     t.text "description"
-    t.string "status"
+    t.string "status", default: "available"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
