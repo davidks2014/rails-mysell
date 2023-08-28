93fff52c28451113c10c13a511d911503ff0c098
leekoksang <davidks2014@gmail.com>
2023-08-28 20:46:20 +0800

    form_tag change to simple_form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,9 @@
 class ItemsController < ApplicationController
   def index
-    # raise
+    @items = Item.all
+
     if params[:search]
-      @items = Item.where("name LIKE ?", "%#{params[:search]}%")
-    else
-      @items = Item.all
+      @items = Item.where("name LIKE ?", "%#{ params[:search][:query]}%")
     end
   end
 end
