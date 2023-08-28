b3c7e69849792e3a1c08c3ed14a6eaa946d7dfb3
Zunnur Hidayah Zahri <zhidayah@hotmail.com>
2023-08-28 23:27:10 +0800

    homepage frontend revising

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @featured_items = Item.all.shuffle.take(5)
+    @featured_items = Item.all.shuffle.take(4)
   end
 end
