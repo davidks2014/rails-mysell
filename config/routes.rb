d437db6d93023c1c9a75d1b6f9165dc2b5e2a74f
leekoksang <davidks2014@gmail.com>
2023-08-28 20:19:03 +0800

    fai version

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :users do
-    resources :items
-  end
 
   resources :items
+  resources :users, only: %i[show]
 
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
