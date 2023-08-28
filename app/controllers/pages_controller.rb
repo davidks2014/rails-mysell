7f81aefaec611d30f623b58038da8c8c5d1efcdf
teckguan ng <renraelreve@gmail.com>
2023-08-28 21:24:22 +0800

    resolve again conflict on page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-<<<<<<< HEAD
-    @featured_items = Item.order("RANDOM()").limit(4)
-=======
     @featured_items = Item.all.shuffle.take(5)
->>>>>>> 51b704f4a53077efb577cf6ed85387baf6b4e888
   end
 end
