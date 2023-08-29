5f938fd381b5b95ffd3bd9df6c08b0d6bd15c309
leekoksang <davidks2014@gmail.com>
2023-08-29 18:37:19 +0800

    clear commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :items do
-    collection do
-      get 'category/:category', to: 'items#category'
-    end
-  end
+  get "items/:category", to: "categories#show"
+  resources :items
 
   resources :users, only: %i[show]
-
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
