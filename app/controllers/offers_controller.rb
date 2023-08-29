c81cea776eea3eea3f8aacb4ed4c55f982f0d406
leekoksang <davidks2014@gmail.com>
2023-08-29 23:44:24 +0800

    add edit status for offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -6,6 +6,7 @@ class OffersController < ApplicationController
   def new
     @item = Item.find(params[:item_id])
     @offer = Offer.new
+    raise
   end
 
   def create
@@ -21,10 +22,25 @@ class OffersController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:item_id])
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:item_id])
+    @offer = Offer.find(params[:id])
+    if @offer.update(offer_params)
+      redirect_to @item, notice: "Offer was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def offer_params
-    params.require(:offer).permit(:price)
+    params.require(:offer).permit(:price, :status)
   end
 
 end
