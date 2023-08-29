6da19c02f1a70f7b94547530240d8acbaf9726a2
leekoksang <davidks2014@gmail.com>
2023-08-29 18:00:56 +0800

    clear commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,8 @@ Rails.application.routes.draw do
   resources :items
   resources :users, only: %i[show]
 
-  resources :items
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :items, only: [:index, :destroy]
 end
