e5628a0cb81a402351375966cd93841aefd7e81f
leekoksang <davidks2014@gmail.com>
2023-09-02 02:58:16 +0800

    do until button

--- app/controllers/user_offers_controller.rb
+++ app/controllers/user_offers_controller.rb
@@ -1,5 +1,20 @@
 class UserOffersController < ApplicationController
   def index
+    @user = current_user
     @offers = policy_scope(Offer).all
+  end
+
+  def approve
+    @offer = Offer.find(params[:id])
+    # Add logic to approve the offer
+    @offer.update(status: 'approved')
+    redirect_to user_offers_path, notice: "Offer approved successfully"
+  end
+
+  def decline
+    @offer = Offer.find(params[:id])
+    # Add logic to decline the offer
+    @offer.update(status: 'declined')
+    redirect_to user_offers_path, notice: "Offer declined successfully"
   end
 end
